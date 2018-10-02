0d557dc753ed0be36212e57d1dc73d62ab2b960d
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-02 15:10:02 +0200

    Einfache Methoden

--- Methoden/Program.cs
+++ Methoden/Program.cs
@@ -10,12 +10,40 @@ namespace Methoden
     {
         static void Main(string[] args)
         {
+            MachEtwas();
 
+            Addieren(12,6);
+            int ergebnis = Addieren(21,99);
 
+            Console.WriteLine(ergebnis);
 
+            // Tupel
+            // var mehrereWerte = MehrereWerte();
+            // Console.WriteLine(mehrereWerte.stimmt);
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
+
+        // void -> kein ergebnis
+        public static void MachEtwas()
+        {
+            Console.WriteLine("Mach etwas ...");
+        }
+
+        public static int Addieren(int zahl1, int zahl2)
+        {
+            return zahl1 + zahl2;
+        }
+
+        // Mehrere Werte ausgeben:
+        // Tupel erst ab .NET 4.7+
+        public static (bool stimmt,int zahl) MehrereWerte()
+        {
+            return (true, 12);
+        }
+
+
+
     }
 }
