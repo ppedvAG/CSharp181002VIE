aa4b8d46dc539c1ada6c6a7fce700a2319515163
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-02 10:59:25 +0200

    Variablen und Konvertieren

--- HalloWelt/Program.cs
+++ HalloWelt/Program.cs
@@ -10,6 +10,45 @@ namespace HalloWelt
     {
         static void Main(string[] args)
         {
+            #region Variable erstellen
+            //int zahl1;
+            //int z1, z2, z3;
+
+            //int zahl2 = 50;
+            //int z4 = 5, z5 = 10, z6 = 20;
+
+            //Console.WriteLine(zahl2);
+
+            //Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+            //// int eingabe = Console.ReadLine(); 
+            #endregion
+
+            #region Konvertieren
+            //// implizit Konvertieren
+
+            //int wert1 = 50;     // Int32
+            //long wert2 = 100;   // Int64
+
+            //wert2 = wert1;      // Da Int32 kleiner als Int64 ist, ist dies erlaubt
+            //// wert1 = wert2;   // Nicht erlaubt, da int Int64 etwas drinnen stehen --könnte--, was für Int32 zu groß oder zu klein ist
+
+            //// Casten
+            //wert2 = 20;         // ein gültiger Wert für Int32
+            //wert1 = (int)wert2; // Konvertiert den Wert
+
+            //// explizit Konvertieren
+
+            //int konvertierterWert = Convert.ToInt32("12345");
+            //Console.WriteLine(konvertierterWert);
+            #endregion
+
+            // cw + TAB + TAB
+            // -> Code-Snippet
+
+            // STRG + K + S   -> Codebereich umschließen
+
+            Console.WriteLine("Hallo Welt");
+            Console.ReadKey();
         }
     }
 }
