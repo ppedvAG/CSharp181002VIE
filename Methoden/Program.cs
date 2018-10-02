a2084a4d609f2b2b253d2ac9c97314480391997c
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-02 14:58:04 +0200

    Init Methoden

---
+++ Methoden/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
