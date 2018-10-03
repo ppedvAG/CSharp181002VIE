b993ca27a842864484a13e2108d9d0c5f097202a
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-03 09:42:36 +0200

    Person Exception

---
+++ OOP/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Person
+    {
+        #region Gültigkeitsspezifizierer
+        //// public       -> Überall sichtbar
+        //// private      -> Nur in der eigenen Klasse sichtbar
+        //// protected    -> Nur in der eigenen Klasse und allen Klassen, die davon erben, sichtbar
+
+        //// internal     -> Nur in der eigenen Assembly sichtbar
+        //// protected interna -> Mischung aus protected und internal
+
+        //// C#7.2:
+        //// private protected -> Protected, das nur in der eigenen Assembly gilt, ansonsten private 
+        #endregion
+
+        public string vorname;
+        public string nachname;
+        public byte alter;
+        public decimal kontostand;
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,6 +10,60 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            #region Person v1
+            //Person p1;                  // Deklaration
+            //Person p2 = new Person();   // Deklaration + Initialisierung
+
+            //p2.vorname = "Tom";
+            //p2.nachname = "Ate";
+
+            //p1 = new Person();
+
+            //p1.vorname = "Anna";
+            //p1.nachname = "Nass";
+            //p1.alter = 250;
+            //p1.kontostand = -20000000;
+
+            //Console.WriteLine(p1.nachname); 
+            #endregion
+
+            #region Exception
+            //Console.WriteLine("Bitte geben Sie eine Zahl ein");
+
+            //int zahl1 = 0;
+            ////try
+            ////{
+            ////    zahl1 = Convert.ToInt32(Console.ReadLine());
+            ////}
+            ////catch(FormatException) // Jede Exception
+            ////{
+            ////    Console.WriteLine("Bitte keine Buchstaben und Sonderzeichen eingeben ! ...");
+            ////}
+            ////catch (OverflowException) // Jede Exception
+            ////{
+            ////    Console.WriteLine("Ihre Zahl ist zu groß oder zu klein ...");
+            ////}
+
+            //// Schnelles Erstellen von einem Try/Catch - Block
+            //// try + TAB + TAB
+
+
+            //try
+            //{
+            //    zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //Console.WriteLine(zahl1*2); 
+            #endregion
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
