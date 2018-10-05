c96769c18a9c00b2ded16d566eef6f119b077be2
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-05 13:34:17 +0200

    DLL und Publish

---
+++ KonsoleDieEineDLLVerwendet/Program.cs
@@ -0,0 +1,21 @@
+﻿using MeineErsteDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsoleDieEineDLLVerwendet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taschenrechner.Add(12, 1);
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
